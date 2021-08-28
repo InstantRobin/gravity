82f523c835471ce3eb9f62bb8791191da73ca65e
Robin Laptop Linux <cryerrobin@gmail.com>
2021-08-27 19:16:48 -0700

    Added planets array to SpacePanel, 2 planets created when initialized Added velocity to Planet Added getters and setters to Planet Added stubs for creating position/velocity changes

--- src/com/company/Planet.java
+++ src/com/company/Planet.java
@@ -2,16 +2,59 @@ package com.company;
 
 public class Planet {
 
-    int mass;
-    int radius;
-    int x;
-    int y;
+    private int mass;
+    private int radius;
+    private int x;
+    private int y;
+    private int dx;
+    private int dy;
 
-    Planet(int mass, int radius, int x, int y){
+    Planet(int mass, int radius, int x, int y, int dx, int dy){
         this.mass = mass;
         this.radius = radius;
         this.x = x;
         this.y = y;
+        this.dx = dx;
+        this.dy = dy;
     }
 
+    public int getMass() {
+        return mass;
+    }
+
+    public int getRad() {
+        return radius;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getDx() {
+        return dx;
+    }
+
+    public void setDx(int dx) {
+        this.dx = dx;
+    }
+
+    public int getDy() {
+        return dy;
+    }
+
+    public void setDy(int dy) {
+        this.dy = dy;
+    }
 }
--- src/com/company/SpacePanel.java
+++ src/com/company/SpacePanel.java
@@ -2,22 +2,50 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class SpacePanel extends JPanel {
 
+    // As long as planets have gravity towards one another, max of 2 planets allowed in array
+    // (this can be fixed once 3 body problem is solved)
+    private ArrayList<Planet> planets = new ArrayList<>();
+
     SpacePanel(){
         super();
+        planets.add(new Planet(50,50,100,100, 10, 0));
+        planets.add(new Planet(100,75,200,300, 0, 0));
     }
 
     // Sets component size to value determined here
     @Override
     public Dimension getPreferredSize() {
-        return new Dimension(250,200);
+        return new Dimension(500,500);
     }
 
     @Override
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-        g.drawOval(50,50,10,10);
+        for (Planet p : planets){
+            g.drawOval(p.getX(),p.getY(),p.getRad(),p.getRad());
+        }
+    }
+
+    public void updatePlanets(){
+        for (Planet p : planets){
+            updateVelocity(p);
+            updatePosition();
+        }
+    }
+
+    private void updateVelocity(Planet planet){
+        for (Planet p : planets){
+            //stub
+        }
+    }
+
+    private void updatePosition(){
+        for (Planet p : planets){
+            //stub
+        }
     }
 }
