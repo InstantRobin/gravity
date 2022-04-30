138004e6511d45fa1a12abe6888d15c05cbb28ef
Robin Laptop Linux <cryerrobin@yahoo.com>
2022-04-30 08:31:08 -0400

    Refactored initialization of SpacePanel to SpacePanelFactory

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,20 +2,17 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionListener;
 
 public class Main {
 
     private static JFrame frame;
-    private static GridBagConstraints c;
-    private static final int fps = 20;
+    private static GridBagConstraints constraints;
+    private static final int fps = 100;
 
     public static void main(String[] args){
-        SpacePanel space = new SpacePanel();
-
         initFrame();
         initGridBag();
-        initSpace(space);
+        SpacePanel space = new SpacePanelFactory(frame, constraints).initSpace();
         startSimulation(space);
     }
 
@@ -28,42 +25,9 @@ public class Main {
     }
 
     private static void initGridBag() {
-        c = new GridBagConstraints();
-        c.gridx = 0;
-        c.gridy = 0;
-    }
-
-    private static void initSpace(SpacePanel space) {
-        addTopButtons(space);
-        addSpacePanel(space);
-        addSpaceClickListener(space);
-    }
-
-    private static void addTopButtons(SpacePanel space) {
-        addTopButton("Clear Stars", e -> space.clearStars());
-        addTopButton("Clear Satellites", e -> space.clearSatellites());
-        addTopButton("Clear All", e -> {
-            space.clearSatellites();
-            space.clearStars();
-        });
-    }
-
-    // Set up constraints to allow Space to fill up rest of screen
-    private static void addSpacePanel(SpacePanel space) {
-        c.fill = GridBagConstraints.BOTH;
-        c.gridwidth = 3;
-        c.weighty = 1;
-        c.gridx = 0;
-        c.gridy = 1;
-        frame.add(space,c);
-        frame.pack();
-    }
-
-    // Enables mouse interaction
-    private static void addSpaceClickListener(SpacePanel space) {
-        SpaceClickListener listener = new SpaceClickListener(space);
-        space.addMouseListener(listener);
-        space.addMouseMotionListener(listener);
+        constraints = new GridBagConstraints();
+        constraints.gridx = 0;
+        constraints.gridy = 0;
     }
 
     private static void startSimulation(SpacePanel space) {
@@ -75,18 +39,4 @@ public class Main {
         timer.start();
     }
 
-    // Places button sequentially at top of screen
-    // Note: Only works properly if things have only been added to top row
-    private static void addTopButton(String str, ActionListener listener){
-        JButton button = new JButton();
-        button.setText(str);
-        button.addActionListener(listener);
-
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.5;
-        c.gridx = frame.getContentPane().getComponentCount();
-        c.gridy = 0;
-
-        frame.add(button,c);
-    }
 }
---
+++ src/com/company/SpacePanelFactory.java
@@ -0,0 +1,67 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class SpacePanelFactory {
+    private static JFrame frame;
+    private static GridBagConstraints constraints;
+    private static SpacePanel space;
+    
+    SpacePanelFactory(JFrame frame, GridBagConstraints constraints){
+        SpacePanelFactory.frame = frame;
+        SpacePanelFactory.constraints = constraints;
+        space = new SpacePanel();
+    }
+
+    public SpacePanel initSpace() {
+        addTopButtons();
+        addSpacePanel();
+        addSpaceClickListener();
+        return space;
+    }
+
+    private void addTopButtons() {
+        addTopButton("Clear Stars", e -> space.clearStars());
+        addTopButton("Clear Satellites", e -> space.clearSatellites());
+        addTopButton("Clear All", e -> {
+            space.clearSatellites();
+            space.clearStars();
+        });
+    }
+
+    // Set up constraints to allow Space to fill up rest of screen
+    private void addSpacePanel() {
+        constraints.fill = GridBagConstraints.BOTH;
+        constraints.gridwidth = 3;
+        constraints.weighty = 1;
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        frame.add(space, constraints);
+        frame.pack();
+    }
+
+    // Enables mouse interaction
+    private void addSpaceClickListener() {
+        SpaceClickListener listener = new SpaceClickListener(space);
+        space.addMouseListener(listener);
+        space.addMouseMotionListener(listener);
+    }
+
+    // Places button sequentially at top of screen
+    // Note: Only works properly if things have only been added to top row
+    private void addTopButton(String str, ActionListener listener){
+        JButton button = new JButton();
+        button.setText(str);
+        button.addActionListener(listener);
+        button.setFont(new Font("Arial", Font.PLAIN, 36));
+
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.weightx = 0.5;
+        constraints.gridx = frame.getContentPane().getComponentCount();
+        constraints.gridy = 0;
+
+        frame.add(button, constraints);
+    }
+}
