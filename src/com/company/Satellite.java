fd4a61d77d1649c7537fe906c1913016da08d1f6
Robin Laptop Linux <cryerrobin@yahoo.com>
2022-05-02 11:13:17 -0400

    Fun glitch makes the satellites randomly gain energy from bouncing, fun to watch!

--- src/com/company/Satellite.java
+++ src/com/company/Satellite.java
@@ -5,6 +5,8 @@ import java.awt.geom.Point2D;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import static java.lang.Math.sqrt;
+
 // Represents a body that experiences gravity but does not exert it
 // Moves according to path of gravity
 public class Satellite extends GravBod {
@@ -17,6 +19,7 @@ public class Satellite extends GravBod {
 
     // greatly increased gravity strength given smaller scale
     protected static final double GRAV_CONST = .06674;
+    protected static final double ELASTICITY = 0.75;
     
     Satellite(double x, double y, int radius, Color color, double dx, double dy){
         super(x, y, radius, color);
@@ -31,20 +34,39 @@ public class Satellite extends GravBod {
     }
 
     public void updatePos(List<Star> stars) {
-        x += dx;
-        y += dy;
-        
+        boolean contact = false;
         // Checks to make sure doesn't clip inside of stars, allows bouncing
         // TODO: Works poorly for smaller stars
+        double futX = x + dx;
+        double futY = y + dy;
         for (Star star : stars){
-            double distX = star.getX() - x;
-            double distY = star.getY() - y;
-            double dist = Math.sqrt(Math.pow(distX,2) +
-                                    Math.pow(distY,2));
-            if (dist <= star.getRad()){
-                double angle = Math.atan2(distY,-distX);
-                x += distX >= 0 ? -1 * (Math.abs(star.getRad() * Math.cos(angle)) - distX) : Math.abs(star.getRad() * Math.cos(angle)) + distX;
-                y += distY >= 0 ? -1 * (Math.abs(star.getRad() * Math.sin(angle)) - distY) : Math.abs(star.getRad() * Math.sin(angle)) + distY;
+            double distX = star.getX() - futX;
+            double distY = star.getY() - futY;
+            double dist = sqrt(Math.pow(distX,2) + Math.pow(distY,2));
+            if (dist == star.getRad()){
+                contact = true;
+                dx = 0;
+                dy = 0;
+                break;
+            } else if (dist <= star.getRad()){
+                contact = true;
+                double angle = Math.atan2(distY,distX);
+
+                double deltaX = Math.abs(star.getRad() * Math.cos(angle)) + distX;
+                double deltaY = Math.abs(star.getRad() * Math.sin(angle)) + distY;
+                x += (distX < 0)
+                        ? deltaX
+                        : -1 * deltaX;
+                y += (distY < 0)
+                        ?  deltaY
+                        : -1 * deltaY;
+
+//                x += (distX < 0)
+//                        ? Math.abs(star.getRad() * Math.cos(angle)) + distX
+//                        : -1 * Math.abs(star.getRad() * Math.cos(angle)) + distX;
+//                y += (distY < 0)
+//                        ?  Math.abs(star.getRad() * Math.sin(angle)) + distY
+//                        : -1 * Math.abs(star.getRad() * Math.sin(angle)) + distY;
                 
                 /*
                  Calculate bounce trajectory
@@ -53,16 +75,25 @@ public class Satellite extends GravBod {
                 */
                 Point2D ball = new Point2D.Double(dx,dy);
                 Point2D norm = new Point2D.Double((x-star.getX())/star.getRad(),(y-star.getY())/star.getRad());
-                double dotProd = 2 * ball.getX() * norm.getX() + ball.getY() * norm.getY();
-                Point2D temp = new Point2D.Double(dotProd * norm.getX(),dotProd * norm.getY());
-                
+                double normSum = sqrt(norm.getX() * norm.getX() + norm.getY() * norm.getY());
+                norm = new Point2D.Double(norm.getX()/normSum, norm.getY() / normSum);
+                double dotProd = 2 * (ball.getX() * norm.getX() + ball.getY() * norm.getY());
+                Point2D res = new Point2D.Double(dotProd * norm.getX(),dotProd * norm.getY());
+//                double vel = sqrt(dx * dx + dy * dy);
+                System.out.println("Coords: " + dx + "," + dy + ": " + sqrt(res.getX()*res.getX() + res.getY() * res.getY()));
+
                 // momentum loss on each bounce
-                double elasticity = 0.75;
-                dx = elasticity *(ball.getX() - temp.getX());
-                dy = elasticity *(ball.getY() - temp.getY());
+                dx = ELASTICITY * (ball.getX() - res.getX());
+                dy = ELASTICITY * (ball.getY() - res.getY());
+                break;
             }
         }
-        
+        if (!contact){
+            x += dx;
+            y += dy;
+            System.out.println(x + "," + y + " : " + (sqrt(dx*dx + dy*dy)));
+        }
+
         if (history.size() > 50) {
             history.remove(0);
         }
@@ -74,7 +105,7 @@ public class Satellite extends GravBod {
         for (Star star : stars){
             double distX = star.getX() - x;
             double distY = star.getY() - y;
-            double dist = Math.sqrt(Math.pow(distX,2) + Math.pow(distY,2));
+            double dist = sqrt(Math.pow(distX,2) + Math.pow(distY,2));
             double angle = Math.atan2(distY,distX);
 
             double acc = GRAV_CONST * star.getMass() / Math.pow(dist,2);
